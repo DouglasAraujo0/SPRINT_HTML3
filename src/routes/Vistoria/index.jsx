b9a21e34e8e375eb84a012a2e97c0e3e3adf8c85
LuizFFarias <rm99519@fiap.com.br>
2023-09-09 02:11:33 -0300

    lista para adicionar fotos criada

---
+++ src/routes/Vistoria/index.jsx
@@ -0,0 +1,36 @@
+
+export default function Vistoria(){
+
+    return(
+        <main>
+            <h1>Vistoria</h1>
+            <div id="fotosVistoria">
+            <h2>Fotos</h2>
+            <p>Envie fotos dos pontos chaves da sua bike listados abaixo</p>
+            <ul>
+                <li>Foto da bike inteira de lado: </li>
+                <input type="text" id="ftBikeInteira" placeholder="Digite aqui"/>
+                <li>Foto do número: </li>
+                <input type="text" id="ftNumero" placeholder="Digite aqui"/>
+                <li>Foto da roda: </li>
+                <input type="text" id="ftRoda" placeholder="Digite aqui"/>
+                <li>Foto dos freios: </li>
+                <input type="text" id="ftFreios" placeholder="Digite aqui"/>
+                <li>Foto do guidão: </li>
+                <input type="text" id="ftGuidao" placeholder="Digite aqui"/>
+                <li>Foto dos pedais: </li>
+                <input type="text" id="ftPedais" placeholder="Digite aqui"/>
+                <li>Foto sua com a bike: </li>
+                <input type="text" id="ftSelfieBike" placeholder="Digite aqui"/>
+                <li>Foto da bike de frente: </li>
+                <input type="text" id="ftBikeFrente" placeholder="Digite aqui"/>
+                <li>Foto dos acessórios (se for visível): </li>
+                <input type="text" id="ftAcessorios" placeholder="Digite aqui"/>
+            </ul>
+            <button type="button" id="enviaFotos" >Enviar</button>
+            </div>
+    
+            
+        </main>
+    )
+}
