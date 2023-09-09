c0b4a64e4510c93fbcb618619c93c034228918b4
LuizFFarias <rm99519@fiap.com.br>
2023-09-09 02:39:40 -0300

    adicionando a página de validação da vistoria

---
+++ src/routes/ValidacaoVistoria/index.jsx
@@ -0,0 +1,15 @@
+export default function ValidacaoVistoria(){
+    return(
+        <main>
+            <h1>Validação da Vistoria</h1>
+        <div>
+            <h2>Como sei o status da minha vistoria?</h2>
+            <p>Você ficará sabendo do andamento de sua vistoria pelo e-mail que você informou no momento do cadastro com a Porto Seguro.
+                Por lá, você poderá receber um e-mail dizendo se está em análise, com documentos faltando, se foi aprovado ou reprovado.
+                Independemente do tipo de e-mail que você receber, lá você receberá informações de como prosseguir com a sua vistoria.
+            </p>
+            <p>Também é possível acompanhar o status da sua vistoria por aqui: </p>
+        </div>
+        </main>
+    )
+}
