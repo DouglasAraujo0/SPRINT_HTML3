3a49cb03277fca526c9b90c39b5f9d9550c55b0d
rafaellabastos1 <rm552425@fiap.com.br>
2023-09-11 15:10:17 -0300

    Adicionei um link de encaminhamento para a próxima etapa

--- src/routes/Simulacao/index.jsx
+++ src/routes/Simulacao/index.jsx
@@ -1,16 +1,16 @@
 function Simulacao() {
     return(
         <>
-        <h1>Tecnobike</h1>
+            <h1>Tecnobike</h1>
 
-        <h2>Simulação</h2>
+            <h2>Simulação</h2>
 
-        <p>Veja aqui uma simulação de como o cliente faria sua vistoria do produto bike. 
-            Ah, claro: como é apenas uma demonstração, não é possível realmente mandar dados ou algo do tipo.</p>
+            <p>Veja aqui uma simulação de como o cliente irá fazer sua vistoria do produto bike. 
+                Ah, claro: como é apenas uma demonstração, não é possível realmente mandar dados ou algo do tipo.</p>
 
-        <p>A vistoria é composta por 5 etapas:</p>
+            <p>A vistoria é composta por 5 etapas:</p>
 
-        <p><b>Primeira etapa: </b> A ecolha do tipo de seguro </p>
+            <p><b><Link to='/Identificacao'>Primeira etapa: </Link></b>Identificação do cliente</p>
         </>
 
     )
