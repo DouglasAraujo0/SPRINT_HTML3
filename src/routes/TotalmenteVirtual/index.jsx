c6da2ea88e0263f4230fabc3b34574c5bebe5354
rafaellabastos1 <rm552425@fiap.com.br>
2023-09-11 15:11:06 -0300

    Arrumei uns erros na escrita

--- src/routes/TotalmenteVirtual/index.jsx
+++ src/routes/TotalmenteVirtual/index.jsx
@@ -1,28 +1,26 @@
 function TotalmenteVirtual() {
+
   return (
     <>
-    <h1>Technobike</h1>
+      <h1>Technobike</h1>
 
-    <h2>Totalmente Virtual</h2>
+      <h2>Totalmente Virtual</h2>
 
-    <h3>Virtualizesse</h3>
+      <h3>Virtualizesse</h3>
 
-    <p>Todas as etapas da nossa vistoria acontecem por meio virtual, sem necessidade de deslocamento e estresse 
-        - seja por parte do funcionário da vistoria ou do cliente.Dessa forma, promove uma diminuição dos custos finais, 
-        e tambem uma diminuição de tempo e de desgaste para ambos, onde tudo é feito com alguns cliques.</p>
+      <p>Todas as etapas da nossa vistoria acontecem por meio virtual, sem necessidade de deslocamento e estresse - 
+          seja por parte do funcionário da vistoria ou do cliente. Dessa forma, promove uma diminuição dos custos 
+          finais e uma diminuição de tempo e de desgaste para ambos, pois tudo é feito com alguns cliques.</p>
 
-    <p>E mais: o que antes levava dias para ser concluído, devido a tantas etapas e análises, é possível fazer em apenas um dia. 
-        Isso porque a Technobike conta com uma inteligência artificial com visão computadorizada ,
-        que faz toda a análise da vistoria em poucos segundos. 
-        Imagina que legal querer assegurar sua bike hoje e já assinar o contrato no mesmo dia, né?</p>
+      <p>E mais: o que antes levava dias para ser concluído, devido a tantas etapas e análises, agora é possível 
+          fazer em apenas um dia. Isso porque a Technobike conta com uma inteligência artificial com visão 
+          computadorizada, que faz toda a análise da vistoria em poucos segundos. Imagina que legal querer assegurar
+          sua bike hoje e já vistoriar no mesmo dia, né?</p>
 
-    <p>Durante todo o processo, o cliente pode entrar em contato com nosso canal de atendimento que é 
-        um ChatBot construído e treinado por nós - para retirar suas dúvidas. 
-        E, ao fim, o cliente também pode enviar um feedback em questão de tempo, serviços, problemas, 
-        atendimento e resolução de dúvidas do seu processo de vistoria.</p>
+      <p>Durante todo o processo, o cliente pode entrar em contato com nosso canal de atendimento que é um ChatBot 
+        construído e treinado por nós para retirar suas dúvidas. E, ao fim, o cliente também pode enviar um feedback
+        em questão de tempo, serviços, problemas, atendimento e resolução de dúvidas do seu processo de vistoria.</p>
     </>
-
-    
   )
 }
 
