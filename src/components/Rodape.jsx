8a109b3ca19bd4fc1b2c4605ad0382bafc3a9556
rafaellabastos1 <rm552425@fiap.com.br>
2023-09-11 15:09:40 -0300

    adicionei algumas pastas que estavam faltando e arrumei o caminho de outra pasta

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -5,17 +5,21 @@ export default function Rodape() {
             <b>Confira mais:</b>
             <Link to='/Detalhes'>Detalhes da solução</Link>
             <span> | </span>
-            <Link to='/IdentificarCliente'>Identificação</Link>
-            <span> | </span>
             <Link to='/Simulacao'>Simulação</Link>
             <span> | </span>
-            <Link to='/'>Totalmente Virtual</Link>
-            <span> | </span>
-            <Link to='/Organograma'>Organograma</Link>
+            <Link to='/Identificacao'>Identificação</Link>
             <span> | </span>
             <Link to='/TipoDoSeguro'>Tipo de Seguro</Link>
             <span> | </span>
             <Link to='/Vistoria'>Vistoria</Link>
+            <span> | </span>
+            <Link to='/ValidacaoVistoria'>Validação da vistoria</Link>
+            <span> | </span>
+            <Link to='/TotalmenteVirtual'>Totalmente Virtual</Link>
+            <span> | </span>
+            <Link to='/Organograma'>Organograma</Link>
+            <span> | </span>
+            <Link to='/Integrantes'>Integrantes</Link>
             <span> | </span>
             <Link to='/'>Página Principal</Link>    
         </nav>
